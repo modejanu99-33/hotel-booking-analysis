fbe995b0dcb6479b4996c63b043363c90f85641e
modejanu99-33 <modejanu99@gmail.com>
2026-06-19 11:28:21 +0300

    Create 01_data_understanding.sql

---
+++ sql/01_data_understanding.sql
@@ -0,0 +1,55 @@
+-- DATA UNDERSTANDING
+
+-- Dataset Overview
+
+SELECT COUNT(*) AS total_bookings
+FROM hotel_bookings;
+
+-- Hotel Distribution
+
+SELECT
+    hotel,
+    COUNT(*) AS bookings
+FROM hotel_bookings
+GROUP BY hotel;
+
+-- Missing Values
+
+SELECT COUNT(*) AS missing_country
+FROM hotel_bookings
+WHERE country IS NULL;
+
+SELECT COUNT(*) AS missing_children
+FROM hotel_bookings
+WHERE children IS NULL;
+
+SELECT COUNT(*) AS missing_agent
+FROM hotel_bookings
+WHERE agent IS NULL;
+
+SELECT COUNT(*) AS missing_company
+FROM hotel_bookings
+WHERE company IS NULL;
+
+-- Data Quality Check
+
+SELECT COUNT(*) AS bookings_without_guests
+FROM hotel_bookings
+WHERE adults = 0
+AND COALESCE(children,0) = 0
+AND COALESCE(babies,0) = 0;
+
+-- Outlier Detection
+
+SELECT MAX(lead_time) AS max_lead_time
+FROM hotel_bookings;
+
+SELECT
+    AVG(adr) AS avg_adr,
+    MAX(adr) AS max_adr
+FROM hotel_bookings;
+
+SELECT
+    PERCENTILE_CONT(0.99)
+    WITHIN GROUP (ORDER BY adr) AS adr_99th_percentile
+FROM hotel_bookings;
