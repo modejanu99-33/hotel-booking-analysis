b42fd227b000b32716920d6bc666b7a6c7a89af9
modejanu99-33 <modejanu99@gmail.com>
2026-06-19 12:43:41 +0300

    Update 02_EDA.sql

--- sql/02_EDA.sql
+++ sql/02_EDA.sql
@@ -1,1 +1,92 @@
+-- EXPLORATORY DATA ANALYSIS
 
+-- Hotel Distribution
+
+SELECT
+    hotel,
+    COUNT(*) AS bookings
+FROM hotel_bookings
+GROUP BY hotel
+ORDER BY bookings DESC;
+
+-- Overall Cancellation Rate
+
+SELECT
+    COUNT(*) AS bookings,
+    SUM(is_canceled) AS cancellations,
+    ROUND(100.0 * AVG(is_canceled),2) AS cancellation_rate
+FROM hotel_bookings;
+
+-- Customer Type Distribution
+
+SELECT
+    customer_type,
+    COUNT(*) AS bookings
+FROM hotel_bookings
+GROUP BY customer_type
+ORDER BY bookings DESC;
+
+-- Top 10 Countries
+
+SELECT
+    country,
+    COUNT(*) AS bookings
+FROM hotel_bookings
+WHERE country IS NOT NULL
+GROUP BY country
+ORDER BY bookings DESC
+LIMIT 10;
+
+-- Deposit Type Distribution
+
+SELECT
+    deposit_type,
+    COUNT(*) AS bookings
+FROM hotel_bookings
+GROUP BY deposit_type
+ORDER BY bookings DESC;
+
+-- Repeat Guest Distribution
+
+SELECT
+    is_repeated_guest,
+    COUNT(*) AS bookings
+FROM hotel_bookings
+GROUP BY is_repeated_guest;
+
+-- Lead Time Overview
+
+SELECT
+    MIN(lead_time),
+    ROUND(AVG(lead_time),2),
+    MAX(lead_time)
+FROM hotel_bookings;
+
+-- Length of Stay Overview
+
+SELECT
+    MIN(stays_in_week_nights + stays_in_weekend_nights) AS min_nights,
+    ROUND(
+        AVG(stays_in_week_nights + stays_in_weekend_nights),
+        2
+    ) AS avg_nights,
+    MAX(stays_in_week_nights + stays_in_weekend_nights) AS max_nights
+FROM hotel_bookings;
+
+-- Previous Cancellations Overview
+
+SELECT
+    previous_cancellations,
+    COUNT(*) AS bookings
+FROM hotel_bookings
+GROUP BY previous_cancellations
+ORDER BY previous_cancellations;
+
+-- Parking Requirement Distribution
+
+SELECT
+    required_car_parking_spaces,
+    COUNT(*) AS bookings
+FROM hotel_bookings
+GROUP BY required_car_parking_spaces
+ORDER BY required_car_parking_spaces;
